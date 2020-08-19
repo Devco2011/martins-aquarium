1113d9299789d8a2b03b6123231ff1b22423f33d
Devco2011 <devco2011@gmail.com>
2020-08-19 15:35:20 -0500

    Many changes on ever file. Just practicing commits right now.

--- scripts/FishDataProvider.js
+++ scripts/FishDataProvider.js
@@ -1,38 +1,94 @@
 const fishCollection = [
     { 
-      name: "Big Blue"
-      species: "Bet"
-      length: "2 inches"
-      diet: "Flies"
-      harvestLocation: "Beaches of Walmart"
+      givenName: "Big Blue",
+      species: "Bet",
+      length: "2 inches",
+      diet: "Flies",
+      harvestLocation: "Beaches of Walmart",
+      image: "./../images/blueFish.jpeg"
 
-    }
+    },
     {
-        name: "Oran"
-      species: "Goldfish"
-      length: "2 inches"
-      diet: "Flies"
-      harvestLocation: "Pond"
+      givenName: "Oran",
+      species: "Goldfish",
+      length: "2 inches",
+      diet: "Flies",
+      harvestLocation: "Pond",
+      image: "./../images/orangeFish.jpeg"
 
-
-    }
+    },
     {
-        name: "Purp"
-      species: "Unknown"
-      length: "3 inches"
-      diet: "Fish food"
-      harvestLocation: "Reef"
+      givenName: "Purp",
+      species: "Unknown",
+      length: "3 inches",
+      diet: "Fish food",
+      harvestLocation: "Reef",
+      image: "./../images/purpleFish.jpeg"
 
 
-    }
+    },
     {
-        name: "Mr. Red"
-      species: "Unknown"
-      length: "2 inches"
-      diet: "Flies"
-      harvestLocation: "Beaches of Walmart"
+      givenName: "Mr. Red",
+      species: "Unknown",
+      length: "2 inches",
+      diet: "Flies",
+      harvestLocation: "Beaches of Walmart",
+      image: "./../images/redFish.jpeg"
 
 
-    }
+    },
+    {
+        givenName: "Dory",
+        species: "Royal Blue Tang",
+        length: "18 cm",
+        diet: "crustaceans",
+        harvestLocation: "Reef",
+        image: "./../images/blueFish.jpeg"
+    },
+    {
+        givenName: "Archy",
+        species: "Arc-Eye Hawkfish",
+        length: "20 cm",
+        diet: "shrimp and other small fishes",
+        harvestLocation: "Seaward reefs in the pacific ocean",
+        image: "./../images/blueFish.jpeg"
+    },
+    {
+        givenName: "Pear",
+        species: "Bartletts' Anthias",
+        length: "9 cm",
+        diet: "zooplankton",
+        harvestLocation: "Coral reef in the western Pacific ocean",
+        image: "./../images/blueFish.jpeg"
+    },
+    {
+        givenName: "Axel Rose",
+        species: "Axilspot Wrasse",
+        length: "20 cm",
+        diet: "mollusks",
+        harvestLocation: "A clear lagoon",
+        image: "./../images/blueFish.jpeg"
+    },
+    {
+        givenName: "Angle",
+        species: "Bicolor Angelfish",
+        length: "15 cm",
+        diet: "coral polyps",
+        harvestLocation: "Coral and rubble areas",
+        image: "./../images/blueFish.jpeg"
+    },
+    {
+        givenName: "Patricia",
+        species: "Bignose Unicornfish",
+        length: "55 cm",
+        diet: "zooplankton",
+        harvestLocation: "Deep lagoon",
+        image: "./../images/blueFish.jpeg"
+    },
     
-]
+];
+
+
+export const useFish = () => {
+    return fishCollection.slice()
+}
