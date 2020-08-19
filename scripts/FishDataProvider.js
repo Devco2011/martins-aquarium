5c71298ee297dc76da69611abe49c7505b392119
Devco2011 <devco2011@gmail.com>
2020-08-19 10:13:13 -0500

    fixed fish info in html and added object array in js

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,38 @@
+const fishCollection = [
+    { 
+      name: "Big Blue"
+      species: "Bet"
+      length: "2 inches"
+      diet: "Flies"
+      harvestLocation: "Beaches of Walmart"
+
+    }
+    {
+        name: "Oran"
+      species: "Goldfish"
+      length: "2 inches"
+      diet: "Flies"
+      harvestLocation: "Pond"
+
+
+    }
+    {
+        name: "Purp"
+      species: "Unknown"
+      length: "3 inches"
+      diet: "Fish food"
+      harvestLocation: "Reef"
+
+
+    }
+    {
+        name: "Mr. Red"
+      species: "Unknown"
+      length: "2 inches"
+      diet: "Flies"
+      harvestLocation: "Beaches of Walmart"
+
+
+    }
+    
+]
