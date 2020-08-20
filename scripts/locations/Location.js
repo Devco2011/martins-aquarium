38a9df64d5bfe407af1e07cb017acb81097d017f
Devco2011 <devco2011@gmail.com>
2020-08-20 10:43:14 -0500

    Automated locations

---
+++ scripts/locations/Location.js
@@ -0,0 +1,12 @@
+export const Location = (locationObj) => {
+    return `
+    <div class="harvestingLocations">
+        
+   <ul>
+       <li> ${locationObj.location}</li>
+       
+       
+   </ul>
+    </div>
+`
+}
--- scripts/tips/TipDataProvider.js
+++ scripts/tips/TipDataProvider.js
@@ -6,6 +6,9 @@ const tipsCollection = [
       tip: "Have daily talks with your fish about vacation."
 
     },
+    {
+        tip: "Sing Jazz standards to your fish in the morning"
+    }
     
 ];
 
